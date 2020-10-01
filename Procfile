a8d8951bd798714c314bcc8c088260787e70872d
Keith Matthews <keith@keithdmatthews.com>
2020-10-01 12:13:08 -0700

    chore - Staging scripts for remote deploy

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: bash release.sh
+web: python app/run.py 0.0.0.0:3001
---
+++ clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -
+
+rm -f data/DisasterResponse.db
+# rm models/classifier.pkl
+# rm models/nlp_vocabulary.joblib
+# rm models/predicted.joblib
+# rm canon.joblib
+rm -f models/*.pkl models/*.joblib
