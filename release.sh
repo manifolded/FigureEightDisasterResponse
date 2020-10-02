1ce1c67649c1dc6ccdd3ab75455791e641d54edc
Keith Matthews <keith@keithdmatthews.com>
2020-10-01 22:03:16 -0700

    debug - RUN not present on Heroku build env

--- release.sh
+++ release.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -
 
-# RUN python -c "import nltk;nltk.download('snowball_data')"
-RUN python -c "import nltk; nltk.download('popular')"
+# python -c "import nltk;nltk.download('snowball_data')"
+python -c "import nltk; nltk.download('popular')"
 
 python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db
 
 python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl
-
