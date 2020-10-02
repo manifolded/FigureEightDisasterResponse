77299b60816ea4e309ddf88104c9a521765442af
Keith Matthews <keith@keithdmatthews.com>
2020-10-02 08:38:16 -0700

    fix - Release execution in subdirs

--- release.sh
+++ release.sh
@@ -3,6 +3,9 @@
 # python -c "import nltk;nltk.download('snowball_data')"
 python -c "import nltk; nltk.download('popular')"
 
-python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db
+pushd data
+python process_data.py disaster_messages.csv disaster_categories.csv DisasterResponse.db
+popd
 
-python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl
+pushd models
+python train_classifier.py ../data/DisasterResponse.db classifier.pkl
