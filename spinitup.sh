68629feba36d6ea3cd923a02c483042d63736306
Keith Matthews <keith@keithdmatthews.com>
2020-10-02 20:31:10 -0700

    fix - Merge release and run phases into just run

--- spinitup.sh
+++ spinitup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -
 
-# python -c "import nltk;nltk.download('snowball_data')"
-python -c "import nltk; nltk.download('popular')"
-
 pushd data
 python process_data.py disaster_messages.csv disaster_categories.csv DisasterResponse.db
 popd
 
 pushd models
 python train_classifier.py ../data/DisasterResponse.db classifier.pkl
+popd
+
+pushd app
+python run.py
+popd
